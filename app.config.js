88a696cd6f2921cf73fe62a740028f73be438a52
Robby Eickhoff <robbyeickhoff@gmail.com>
2026-05-25 08:43:02 -0600

    Load Google Maps key from environment config

--- app.config.js
+++ app.config.js
@@ -3,6 +3,16 @@ const appJson = require("./app.json");
 module.exports = ({ config }) => ({
   ...config,
   ...appJson.expo,
+  android: {
+    ...appJson.expo.android,
+    config: {
+      ...appJson.expo.android?.config,
+      googleMaps: {
+        ...appJson.expo.android?.config?.googleMaps,
+        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
   extra: {
     ...appJson.expo.extra,
     mapboxPublicToken: process.env.EXPO_PUBLIC_MAPBOX_TOKEN,
